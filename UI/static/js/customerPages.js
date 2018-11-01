46eca942ebefe70a1af89f5326e691222213b29e
Elmonstro <jratcher.gmail>
2018-11-01 19:04:53 +0300

    Add a function to check if dictionary is empty

---
+++ UI/static/js/customerPages.js
@@ -0,0 +1,13 @@
+'use strict'
+var allOrders = {}
+
+// Fuction to check if tray is empty
+function isEmpty(dict){
+    return Object.getOwnPropertyNames(dict) == 0;
+}
+
+// Function to display order from
+function DisplayOrders(){
+    ;
+
+}
