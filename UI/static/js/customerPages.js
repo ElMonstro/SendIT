be8d710420e2eb8420899a9ec526082163485b4d
Elmonstro <jratcher.gmail>
2018-11-01 19:12:03 +0300

    Add orders object

--- UI/static/js/customerPages.js
+++ UI/static/js/customerPages.js
@@ -1,5 +1,9 @@
 'use strict'
-var allOrders = {}
+var allOrders = {
+    321: ['4 5345 343', '4 5343 343', 5, 445, 'In-transit'],
+    453: ['4 5435 324', '6 5356 353', 3, 674, 'Delivered'],
+    133: ['5 6535 453', '8 5465 742', 6, 546,  'Canceled']
+}
 
 // Fuction to check if tray is empty
 function isEmpty(dict){
@@ -8,6 +12,6 @@ function isEmpty(dict){
 
 // Function to display order from
 function DisplayOrders(){
-    ;
+    
 
 }
