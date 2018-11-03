e8946ad608ade4ac56665464f94d1263fdeb0389
Elmonstro <jratcher.gmail>
2018-11-03 10:56:36 +0300

    Add logic to populate order details depending on mode and user

--- UI/static/js/customerPages.js
+++ UI/static/js/customerPages.js
@@ -2,6 +2,9 @@
 
 /* Global variables*/
 var pricePerKg = 200;
+// display modes
+var edit = 'edit';
+var view = 'view';
 // Display order options
 var all = 'all';
 var canceled = 'Canceled';
@@ -33,42 +36,42 @@ const adminDeliveredOption = document.querySelector('#admin-deliver')
 var allOrders = {
     321: ['4 5345 343', '4 5343 343', 5, 'In-transit'],
     453: ['4 5435 324', '6 5356 353', 3, 'Delivered'],
-    133: ['5 6535 453', '8 5465 742', 6,  'Canceled'],
+    133: ['5 6535 453', '8 5465 742', 6, 'Canceled'],
     301: ['4 5345 343', '4 5343 343', 5, 'In-transit'],
-    353: ['4 5435 324', '6 5356 353', 3,  'Delivered'],
-    633: ['5 6535 453', '8 5465 742', 6,  'Canceled']
+    353: ['4 5435 324', '6 5356 353', 3, 'Delivered'],
+    633: ['5 6535 453', '8 5465 742', 6, 'Canceled']
 }
 
 var allOrdersAdmin = {
     321: ['4 5345 343', '4 5343 343', 5, 'In-transit'],
     453: ['4 5435 324', '6 5356 353', 3, 'Delivered'],
-    133: ['5 6535 453', '8 5465 742', 6,  'Canceled'],
+    133: ['5 6535 453', '8 5465 742', 6, 'Canceled'],
     301: ['4 5345 343', '4 5343 343', 5, 'In-transit'],
-    353: ['4 5435 324', '6 5356 353', 3,  'Delivered'],
-    633: ['5 6535 453', '8 5465 742', 6,  'Canceled'],
+    353: ['4 5435 324', '6 5356 353', 3, 'Delivered'],
+    633: ['5 6535 453', '8 5465 742', 6, 'Canceled'],
     365: ['4 5345 343', '4 5343 343', 5, 'In-transit'],
     495: ['4 5435 324', '6 5356 353', 3, 'Delivered'],
-    127: ['5 6535 453', '8 5465 742', 6,  'Canceled'],
+    127: ['5 6535 453', '8 5465 742', 6, 'Canceled'],
     249: ['4 5345 343', '4 5343 343', 5, 'In-transit'],
-    132: ['4 5435 324', '6 5356 353', 3,  'Delivered'],
-    808: ['5 6535 453', '8 5465 742', 6,  'Canceled']
+    132: ['4 5435 324', '6 5356 353', 3, 'Delivered'],
+    808: ['5 6535 453', '8 5465 742', 6, 'Canceled']
 }
 
 // Fuction to check if tray is empty
-function isEmpty(dict){
+function isEmpty(dict) {
     return Object.getOwnPropertyNames(dict) == 0;
 }
 
 // Function to display order from
-function DisplayOrders(user, option){
+function DisplayOrders(user, option) {
     currentOption = option;
     allOrdersDiv.innerHTML = '';
     var actionButton = '<span class="action-btn cancel-btn"><img src="static/img/cancel.png"></span>'
-    if (user == admin){
+    if (user == admin) {
         allOrders = allOrdersAdmin;
         actionButton = '<span class="action-btn edit-btn" >Edit</span>'
     }
-    for(var order in allOrders){
+    for (var order in allOrders) {
         var pickupAdd = allOrders[order][0];
         var destAdd = allOrders[order][1];
         var weight = allOrders[order][2];
@@ -76,125 +79,154 @@ function DisplayOrders(user, option){
         var status = allOrders[order][3];
         const orderDiv = document.createElement('div');
         orderDiv.className = 'order';
-        orderDiv.innerHTML = 
-        `<span class="order-id">${order}</span>
+        orderDiv.innerHTML =
+            `<span class="order-id">${order}</span>
         <span class="pickup">${pickupAdd}</span>
         <span class="Destination">${destAdd}</span>
         <span><span class="weight">${weight}</span> Kgs</span>
         <span><span>Kshs</span> <span class="price"> ${price}</span></span>
         <span class="statuses"><span class="status">${status}</span>${actionButton}</span></span>`;
-        
+
 
         var orderId = orderDiv.querySelector('.order-id');
-        orderDiv.addEventListener('click', ()=>{
+        orderDiv.addEventListener('click', () => {
             viewOrder();
         });
 
         const statusSpan = orderDiv.querySelector('.status');
-        const actionBtn =  orderDiv.querySelector('.action-btn');
+        const actionBtn = orderDiv.querySelector('.action-btn');
 
         actionBtn.addEventListener('click',
-        ()=>{
-            console.log('edited')
-        })
+            () => {
+                console.log('edited')
+            })
 
         // Display different colors for different status
-        if (status == canceled){
-            statusSpan.classList.add('canceled')            
+        if (status == canceled) {
+            statusSpan.classList.add('canceled')
         }
-        if(status == inTransit){
+        if (status == inTransit) {
             actionBtn.style.display = 'grid'
-            statusSpan.classList.add('in-transit')           
-         }
-         if(status == delivered){
+            statusSpan.classList.add('in-transit')
+        }
+        if (status == delivered) {
             statusSpan.classList.add('delivered')
-        }  
-        
-        if (option == all){
-            allOrdersDiv.appendChild(orderDiv);
         }
-        if (status == option){
-            allOrdersDiv.appendChild(orderDiv);
-        }
-        
-        
 
-    }    
+        if (option == all) {
+            allOrdersDiv.appendChild(orderDiv);
+        }
+        if (status == option) {
+            allOrdersDiv.appendChild(orderDiv);
+        }
+
+
+
+    }
 
 }
 
 
-    
+
 
 // Add event listeners to side-panel options and order stats
-function AddEventListeners(user){
+function AddEventListeners(user) {
 
-    if (user == client){
-    // Client dashboard options
-    transitOption.addEventListener('click', ()=>{
-        DisplayOrders(client, inTransit)
-    });
+    if (user == client) {
+        // Client dashboard options
+        transitOption.addEventListener('click', () => {
+            DisplayOrders(client, inTransit)
+        });
 
-    deliveredOption.addEventListener('click', ()=>{
-        DisplayOrders(client, delivered)
-    });
+        deliveredOption.addEventListener('click', () => {
+            DisplayOrders(client, delivered)
+        });
 
-    canceledOption.addEventListener('click', ()=>{
-        DisplayOrders(client, canceled)
-    });
+        canceledOption.addEventListener('click', () => {
+            DisplayOrders(client, canceled)
+        });
 
-    allOption.addEventListener('click', ()=>{
-        DisplayOrders(client, all)
-    });
+        allOption.addEventListener('click', () => {
+            DisplayOrders(client, all)
+        });
     }
-   
-    if (user == admin){
-    // Admin dashboard options
-    adminTransitOption.addEventListener('click', ()=>{
-        DisplayOrders(admin, inTransit)
-    });
 
-    adminDeliveredOption.addEventListener('click', ()=>{
-        DisplayOrders(admin, delivered)
-    });
+    if (user == admin) {
+        // Admin dashboard options
+        adminTransitOption.addEventListener('click', () => {
+            DisplayOrders(admin, inTransit)
+        });
 
-    adminCanceledOption.addEventListener('click', ()=>{
-        DisplayOrders(admin, canceled)
-    });
+        adminDeliveredOption.addEventListener('click', () => {
+            DisplayOrders(admin, delivered)
+        });
 
-    adminAllOption.addEventListener('click', ()=>{
-        DisplayOrders(admin, all)
-    });
+        adminCanceledOption.addEventListener('click', () => {
+            DisplayOrders(admin, canceled)
+        });
+
+        adminAllOption.addEventListener('click', () => {
+            DisplayOrders(admin, all)
+        });
 
     }
-   
+
     // Order statistics event listener for both admin and client dashboards
-    orderStatistics.addEventListener ('click', (e)=>{
-        if (e.target.id == 'dlvd'){
-            DisplayOrders(user, delivered);          
+    orderStatistics.addEventListener('click', (e) => {
+        if (e.target.id == 'dlvd') {
+            DisplayOrders(user, delivered);
         }
-        if (e.target.id == 'cncld'){
-            DisplayOrders(user, canceled);          
+        if (e.target.id == 'cncld') {
+            DisplayOrders(user, canceled);
         }
-        if (e.target.id == 'trst'){
-            DisplayOrders(user, inTransit);            
+        if (e.target.id == 'trst') {
+            DisplayOrders(user, inTransit);
         }
     });
 }
 
 // Function to view a single order
 
-function viewOrder(user, mode){
+function viewOrder(user, mode) {
     allOrdersDiv.innerHTML = ''
     ordersTitle.style.display = 'none';
     allOrdersDiv.style.marginTop = '50px';
-    const singleOrder = document.createElement('div')
+    const singleOrder = document.createElement('div');
     singleOrder.id = 'single-order';
-    singleOrder.innerHTML = 
-    `<span class="heading">Order Number: <span id="order-no">435</span></span>
+    singleOrder.className = 'single-order'
+
+    var destLocationHtml = '<span id="">3-5334-533</span>';
+    var currentLocationHtml = '<span class="content">Mai Mahiu</span>';
+    var statusHtml = '<span id="stts-color" class="in-transit">In-transit</span>';
+    var editModeInputLabel = '';
+    var editModeInputLabelAdmin = '';
+    
+
+    if (mode == view) {
+        singleOrder.classList.add('view-mode')
+    }
+
+    if (mode == edit && user == client) {
+        editModeInputLabel = 'edit-input-label'
+        destLocationHtml = '<input id="dest-input" class="edit-input" type="text" value="3-5334-533">';
+    }
+
+    if (mode == edit && user == admin) {
+        editModeInputLabelAdmin = 'edit-input-label'
+        singleOrder.id = 'admin-single-order';
+        singleOrder.className = 'single-order'
+        currentLocationHtml = '<input id="current-input" class="edit-input" type="text" value="Kisii">';
+        statusHtml = ` <span><span><select>
+        <option value="intransit">In-transit</option>
+        <option value="delivered">Delivered</option>
+      </select></span> <span id="save-status-btn" class="invincible">Save</span></span></span> `;
+
+    }
+    singleOrder.innerHTML =
+        `<span class="heading">Order Number: <span id="order-no">435</span></span>
     <div id="delivery-stts" class="detail">
         <span class="label">Delivery status:</span>
-        <span class="content "><span id="stts-color" class="in-transit">In-transit</span></span>
+        <span class="content ">${statusHtml}</span>
     </div>
     <div id="recpnt-name" class="detail">
         <span class="label">Recepient Name:</span>
@@ -209,19 +241,22 @@ function viewOrder(user, mode){
         <span class="content">5-5332-532</span>
     </div>
     <div id="current-location" class="detail">
-        <span class="label">Current Locaton:</span>
-        <span class="content">Mai Mahiu</span>
+        <span class="label ${editModeInputLabelAdmin}">Current Locaton:</span>
+        ${currentLocationHtml}
     </div>
     <div id="dest-location" class="detail">
-        <span class="label">Destination Location:</span>
-        <span class="content"><input id="dest-input" class="edit-input" type="text" value="3-5334-533"><span><span id="save-btn" class="invincible">Save</span></span></span>
+        <span class="label ${editModeInputLabel}">Destination Location:</span>
+<span class="content">${destLocationHtml}<span><span id="save-btn" class="invincible">Save</span></span></span>
     </div>
     </div>`
     var inputDiv = singleOrder.querySelector('#dest-input');
     var saveBtn = singleOrder.querySelector('#save-btn');
-    inputDiv.addEventListener('input', ()=>{
-        saveBtn.style.display = 'grid';       
-    });
+
+    if (mode == edit && user == client) {
+        inputDiv.addEventListener('input', () => {
+            saveBtn.style.display = 'grid';
+        });
+    }
     allOrdersDiv.appendChild(singleOrder);
 
 }
@@ -229,13 +264,14 @@ function viewOrder(user, mode){
 
 // Listen to DOMContentLoaded event
 
-document.addEventListener('DOMContentLoaded', () =>{    
-    DisplayOrders(admin, all);
-    if (pageTitle == 'Admin Dashboard'){
-       AddEventListeners(admin);        
-    }else{
-       AddEventListeners(client);
+document.addEventListener('DOMContentLoaded', () => {
+    // DisplayOrders(admin, all);
+    viewOrder(admin, edit);
+    if (pageTitle == 'Admin Dashboard') {
+        AddEventListeners(admin);
+    } else {
+        AddEventListeners(client);
     }
-    
-    
+
+
 });
